f9ebe4e8aa059baab48847823e5757c607cd59ee
Hasret Karci <83207900+KintheN-git@users.noreply.github.com>
2024-10-15 09:05:07 +0300

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://akdulumkumocagi.com", // Projenin ana URL'si
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://akdulumkumocagi.com", // Projenin ana URL'si
   generateRobotsTxt: true, // Robots.txt oluşturulsun
   sitemapSize: 7000,
   exclude: ["/admin/*"], // Belirli sayfaları dışlamak istersen
